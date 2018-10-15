78757dba001e6843a196d983e0516610b6d88bf9
johnwoodill <johnwoodill@gmail.com>
2018-10-15 08:13:00 -0600

    finished chapter 2

--- compile_doc.R
+++ compile_doc.R
@@ -1,4 +1,4 @@
-setwd("/Users/john/Projects/Intro to Stat Learning/")
+setwd("/Users/john/Projects/Notes-Intro-to-Stat-Learning/")
 
 # Header file with abstract
 top <- "/Users/john/Projects/Intro to Stat Learning/header.tex"
@@ -7,9 +7,15 @@ top <- "/Users/john/Projects/Intro to Stat Learning/header.tex"
 doc <-  "/Users/john/Projects/Intro to Stat Learning/notes.txt"
 
 # Merge config file
-system("cat header.tex document.txt > output.tex")
+system("cat header.tex notes.txt > Notes-Intro-to-Stat-Learning.tex")
 
-write(paste0("\\end{document}"), "output.tex", append = TRUE)
+write(paste0("\\end{document}"), "Notes-Intro-to-Stat-Learning.tex", append = TRUE)
 
 # Compile pdf
-system("pdflatex output.tex")
+system("pdflatex Notes-Intro-to-Stat-Learning.tex")
+
+# Remove Latex Files
+system("rm Notes-Intro-to-Stat-Learning.tex")
+system("rm Notes-Intro-to-Stat-Learning.log")
+system("rm Notes-Intro-to-Stat-Learning.aux")
+
