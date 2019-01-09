b10d5222ba8cfe0751ebb410c40c5e08419ddd71
johnwoodill <johnwoodill@gmail.com>
2019-01-09 07:04:23 -0700

    spell check

--- compile_doc.R
+++ compile_doc.R
@@ -1,5 +1,6 @@
 pwd <- getwd()
 setwd(pwd)
+
 # Header file with abstract
 top <- paste0(pwd, "/header.tex")
 
@@ -7,15 +8,14 @@ top <- paste0(pwd, "/header.tex")
 doc <-  paste0(pwd, "/notes.txt")
 
 # Merge config file
-system("cat header.tex notes.txt > Notes-Intro-to-Stat-Learning.tex")
+system("cat header.tex notes.txt > Notes-Hands-On-ML-with-scikit-learn-and-TF.tex")
 
-write(paste0("\\end{document}"), "Notes-Intro-to-Stat-Learning.tex", append = TRUE)
+write(paste0("\\end{document}"), "Notes-Hands-On-ML-with-scikit-learn-and-TF.tex", append = TRUE)
 
 # Compile pdf
-system("pdflatex Notes-Intro-to-Stat-Learning.tex")
+system("pdflatex Notes-Hands-On-ML-with-scikit-learn-and-TF.tex")
 
 # Remove Latex Files
-system("rm Notes-Intro-to-Stat-Learning.tex")
-system("rm Notes-Intro-to-Stat-Learning.log")
-system("rm Notes-Intro-to-Stat-Learning.aux")
-# system("rm Notes-Intro-to-Stat-Learning.toc")
+system("rm Notes-Hands-On-ML-with-scikit-learn-and-TF.tex")
+system("rm Notes-Hands-On-ML-with-scikit-learn-and-TF.log")
+system("rm Notes-Hands-On-ML-with-scikit-learn-and-TF.aux")
